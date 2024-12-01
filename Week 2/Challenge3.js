8bd43593bb0b60e33322e48473a5dde7c95c4334
sara-c9 <sara.caputo.1994@gmail.com>
2024-12-01 17:31:38 +0000

    challenge 3

---
+++ Week 2/Challenge3.js
@@ -0,0 +1,34 @@
+//Create a  User class:
+class User {
+  //Add to the class a property with the name of username.
+  constructor(username) {
+    this._username = username;
+  }
+
+  //Create a setter method that can set the value of the username.
+  set username(username) {
+    this._username = username;
+  }
+}
+
+// Create the Admin class that inherits the User class:
+
+class Admin extends User {
+  //Add a method by the name of expressYourRole(), and make it return the string: "Admin"
+
+  expressYourRole() {
+    return "Admin";
+  }
+
+  //Add to the Admin class another method, sayHello(), that returns the string "Hello admin, XXX"
+  sayHello() {
+    return `Hello admin, ${this._username}`;
+  }
+}
+
+//Create an object admin out of the class Admin:
+//Set its name to "Balthazar" and say hello to the user.
+const admin = new Admin();
+admin.username = "Balthazar";
+
+console.log(admin.sayHello());
